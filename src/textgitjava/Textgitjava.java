b6e261299d925b4bc9c941fe3b5d78c15e20c8f6
zefri22 <ze_fri@ymail.com>
2014-01-26 10:54:32 +0700

    Update Textgitjava.java
    edit yang pertama

--- src/textgitjava/Textgitjava.java
+++ src/textgitjava/Textgitjava.java
@@ -16,5 +16,6 @@ public class Textgitjava {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Tulisan Ku");
+        System.out.println("Tulisan Ku");
     }
 }
